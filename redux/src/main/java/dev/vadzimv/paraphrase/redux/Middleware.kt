ab1cc7d95bdd6d664ef11fc954af92157954b0a1
VysotskiVadim <vadzim.vysotski@mapbox.com>
2023-01-29 15:37:37 +0100

    refactoring

--- redux/src/main/java/dev/vadzimv/paraphrase/redux/Middleware.kt
+++ redux/src/main/java/dev/vadzimv/paraphrase/redux/Middleware.kt
@@ -1,11 +1,3 @@
 package dev.vadzimv.paraphrase.redux
 
-//fun interface Middleware<TState> {
-//    fun next(
-//        getState: () -> TState,
-//        dispatcher: Dispatcher,
-//        action: Action
-//    )
-//}
-
 typealias Middleware<State> = (store: Store<State>) -> (next: Dispatcher) -> (action: Action) -> Unit
--- redux/src/main/java/dev/vadzimv/paraphrase/redux/RootState.kt
+++
@@ -1,4 +0,0 @@
-package dev.vadzimv.paraphrase.redux
-
-interface RootState {
-}
